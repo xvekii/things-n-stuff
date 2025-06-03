c4685995864192e024970bbab195862768743681
xvekii <vedran.dautanec@gmail.com>
2025-06-03 18:50:36 +0200

    Add hamburgerMenuBtn eventListener

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./styles.css";
+const hamburgerMenuBtn = document.querySelector(".hamburger");
+const containerLeft = document.querySelector(".container-left");
+
+hamburgerMenuBtn.addEventListener("click", () => {
+  containerLeft.classList.toggle("active");
+});
