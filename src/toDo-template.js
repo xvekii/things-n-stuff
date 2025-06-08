c38270eec008a994e35a3adf4e19bd073269f41f
xvekii <vedran.dautanec@gmail.com>
2025-06-08 17:25:23 +0200

    Change id to class, add extra note creation

--- src/toDo-template.js
+++ src/toDo-template.js
@@ -1,19 +1,22 @@
 const TITLE = "Title";
 const NOTE = "Write a note";
 
-export function showInitialTodo() {
+export function createTodo() {
   const newTodoCard = document.createElement("div");
   const newTitle = document.createElement("input");
   const newNote = document.createElement("input");
 
+  const newBtnContainer = document.createElement("div");
+  newBtnContainer.classList.add("todo-btn-container");
+
   newTitle.setAttribute("type", "text");
-  newTitle.setAttribute("id", "title");
+  newTitle.setAttribute("class", "title-text");
   newTitle.setAttribute("name", "title");
   newTitle.setAttribute("placeholder", "Title");
   newTitle.setAttribute("autocomplete", "off");
 
   newNote.setAttribute("type", "text");
-  newNote.setAttribute("id", "note");
+  newNote.setAttribute("class", "note-text");
   newNote.setAttribute("name", "note");
   newNote.setAttribute("placeholder", "Write a note...");
   newNote.setAttribute("autocomplete", "off");
@@ -30,6 +33,23 @@ export function showInitialTodo() {
 
   newTodoCard.appendChild(newTitle);
   newTodoCard.appendChild(newNote);
+  newTodoCard.appendChild(newBtnContainer);
 
+  newNote.addEventListener("input", (e) => {
+    const target = e.target;
+    const inputTextlength = target.value.length;
+
+    if (inputTextlength == 20) {
+      const extraNote = document.createElement("input");
+      extraNote.setAttribute("type", "text");
+      extraNote.setAttribute("class", "note-text");
+      extraNote.setAttribute("name", "note");
+      extraNote.setAttribute("autocomplete", "off");
+      extraNote.classList.add("note", "no-border");
+      extraNote.contentEditable = "true";
+      newTodoCard.appendChild(extraNote);
+      extraNote.focus();
+    }
+  });
   return newTodoCard;
 }
