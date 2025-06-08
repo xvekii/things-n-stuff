cb242e7ae06c1eb9c23a1f5a6f17a878ea06e5b5
xvekii <vedran.dautanec@gmail.com>
2025-06-08 10:25:48 +0200

    Replace p elements with inputs, set attributes

--- src/toDo-template.js
+++ src/toDo-template.js
@@ -3,12 +3,24 @@ const NOTE = "Write a note";
 
 export function showInitialTodo() {
   const newTodoCard = document.createElement("div");
-  const newTitle = document.createElement("p");
-  const newNote = document.createElement("p");
+  const newTitle = document.createElement("input");
+  const newNote = document.createElement("input");
+
+  newTitle.setAttribute("type", "text");
+  newTitle.setAttribute("id", "title");
+  newTitle.setAttribute("name", "title");
+  newTitle.setAttribute("placeholder", "Title");
+  newTitle.setAttribute("autocomplete", "off");
+
+  newNote.setAttribute("type", "text");
+  newNote.setAttribute("id", "note");
+  newNote.setAttribute("name", "note");
+  newNote.setAttribute("placeholder", "Write a note...");
+  newNote.setAttribute("autocomplete", "off");
   
   newTodoCard.classList.add("todo");
-  newTitle.classList.add("placeholder", "title");
-  newNote.classList.add("placeholder", "note");
+  newTitle.classList.add("title", "no-border");
+  newNote.classList.add("note", "no-border");
 
   newTitle.contentEditable = "true";
   newNote.contentEditable = "true";
