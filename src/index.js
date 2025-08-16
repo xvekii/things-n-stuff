1e2733560319900315ff3930d25bb6323cad3022
xvekii <vedran.dautanec@gmail.com>
2025-08-16 17:52:59 +0200

    Import todos, define renderTodo, add eventListener to containerRight for opening stored todos on click

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { createTodo } from "./toDo-template.js";
+import { todos } from "./toDo-template.js";
 
 const hamburgerMenuBtn = document.querySelector(".hamburger");
 const containerLeft = document.querySelector(".container-left");
@@ -12,9 +13,34 @@ hamburgerMenuBtn.addEventListener("click", () => {
 
 addToDoBtn.addEventListener("click", () => {
   const toDoCard = createTodo();
-  console.log(toDoCard.newTodoCard);
-  containerRight.appendChild(toDoCard.newTodoCard);
+  renderTodo(toDoCard);
 });
 
+containerRight.addEventListener("click", (e) => {
+  const todo = e.target.closest(".todo"); 
+  
+  if (todo) {
+    const title = todo.querySelector("input[data-title-id]");
+    const titleID = title.dataset.titleId;
+    console.log(titleID);
 
+    const clickedToDo = createTodo();
+    const titleInput = clickedToDo.querySelector("#title");
+    // Get title from AllTodos with titleID
+    const retrievedTodos = todos.getTodos();
+    console.log(retrievedTodos);
+    
+    for (const obj of retrievedTodos) {
+      if (obj.ID == titleID) {
+        titleInput.value = obj.title;
+      }
+    }
+
+    renderTodo(clickedToDo);
+  }
+});
+
+function renderTodo(todo) {
+  containerRight.appendChild(todo);
+}
 
