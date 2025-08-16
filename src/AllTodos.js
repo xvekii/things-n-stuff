374ee051ae75718d5814e5c7ed90c63de3984db0
xvekii <vedran.dautanec@gmail.com>
2025-08-16 17:51:23 +0200

    Define getTodos

--- src/AllTodos.js
+++ src/AllTodos.js
@@ -6,4 +6,11 @@ export class AllTodos {
   addTodo(todo) {
     this.todosArr.push(todo);
   }
+
+  getTodos() {
+    return this.todosArr;
+  }
+  
+  // Add check for existing todo
+  // Upon editing an existing todo, check its place in todosArr
 }
