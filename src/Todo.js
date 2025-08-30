d6691a84befbf7b16639b09aa1c1747d1f0a40db
xvekii <vedran.dautanec@gmail.com>
2025-08-30 14:19:48 +0200

    Add currentStyle

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,12 @@
 export class Todo {
-  constructor(priority, title, notes, dueDate) {
+  constructor(priority, title, notes, dueDate, currentStyle) {
     this.title = title;
     this.notes = notes;
     this.dueDate = dueDate;
     this.priority = priority;
     // this.project = project;
     this.ID = crypto.randomUUID();
-    // this.textNote = false;
-    // this.checkboxNote = false;
+    this.currentStyle = currentStyle;
   }
 
   // editTodo(title, notes) {
