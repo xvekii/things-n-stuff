a043319d22041a8f5d05aa24b97c47377103a610
xvekii <vedran.dautanec@gmail.com>
2025-09-21 20:21:33 +0200

    Add createBtn, tidy up the code

--- src/helpers.js
+++ src/helpers.js
@@ -1,44 +1,57 @@
-
-
 export const createDiv = ({ classes }) => {
   const div = document.createElement("div");
+  div.classList.add(...classes);
   
-  if (classes) {
-    div.classList.add(...classes);
-  }
   return div;
 }
 
 export const createSpan = ({ classes }) => {
   const span = document.createElement("span");
-
-  if (classes) {
-    span.classList.add(...classes);
-  }
+  span.classList.add(...classes);
+  
   return span;
 }
 
 export const createInput = ({ classes, attrs }) => {
   const titleInput = document.createElement("input");
-
-  if (classes) {
-    titleInput.classList.add(...classes);
-  }
+  titleInput.classList.add(...classes);
 
   Object.entries(attrs).forEach(([key, value]) => {
     titleInput.setAttribute(key, value);
   });
+ 
   return titleInput;
 }
 
-
-
 export const createHeading = ({ classes, headLvl, text }) => {
   const heading = document.createElement(headLvl);
+  heading.classList.add(...classes);
   heading.textContent = text;
 
-  if (classes) {
-    heading.classList.add(...classes);
-  }
   return heading;
+}
+
+export const createBtn = ({ classes, attrs, imgSrc, imgClass, imgAlt, text }) => {
+  const button = document.createElement("button");
+  button.classList.add(...classes);
+  
+  if (imgSrc) {
+    const image = document.createElement("img");
+    if (imgClass) {
+      image.classList.add(imgClass);
+    }
+    image.src = imgSrc;
+    image.alt = imgAlt;
+    button.appendChild(image);
+  }
+  
+  if (text) {
+    button.textContent = text;
+  }
+
+  Object.entries(attrs).forEach(([key, value]) => {
+    button.setAttribute(key, value);
+  });
+  
+  return button;
 }
